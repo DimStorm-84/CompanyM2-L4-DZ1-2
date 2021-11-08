27694dc0b6f2263aeeef7faa6c434734b3e0b3de
DimStorm-84 <81226815+DimStorm-84@users.noreply.github.com>
2021-11-08 21:41:41 +0300

    Mod2 Lesson4 DZ1-2

---
+++ src/components/users.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Pagination from "./pagination";
+import User from "./user";
+import { paginate } from "../utils/paginate";
+
+const Users = ({ users: allUsers, ...rest }) => {
+    const count = allUsers.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = (pageIndex) => {
+        console.log("page:", pageIndex);
+        setCurrentPage(pageIndex);
+    };
+    const users = paginate(allUsers, currentPage, pageSize);
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map((user) => (
+                            <User
+                                key={user._id}
+                                user={user}
+                                onDelete={rest.onDelete}
+                                onBookMarkToggle={rest.onBookMarkToggle}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    "users.map": PropTypes.array
+};
+
+export default Users;
